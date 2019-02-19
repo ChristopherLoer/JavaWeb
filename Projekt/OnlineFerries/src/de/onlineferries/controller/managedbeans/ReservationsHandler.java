ca08694a53c45a15052517668d1db1e3dbec2ca4
PFABER <pfaber@git.de>
2019-02-19 20:24:47 +0100

    changeReservation
    Die Reservierungen können geändert werden, es werden aber noch nicht
    alle Daten in der entsprechenden Ansicht angezeigt
    
    Es fehlt noch die Speicherung der Änderungen

--- Projekt/OnlineFerries/src/de/onlineferries/controller/managedbeans/ReservationsHandler.java
+++ Projekt/OnlineFerries/src/de/onlineferries/controller/managedbeans/ReservationsHandler.java
@@ -7,16 +7,22 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
 
 import de.onlineferries.model.service.ReservationService;
 import de.onlineferries.view.ReservationView;
+import de.onlineferries.view.TravellerView;
 
 @ManagedBean
 @SessionScoped
 public class ReservationsHandler implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private int selectedReservation;
+	private String selectedReservation;
+	private String[] reservationValues;
+	private ReservationView reservation;
+	private int travellers;
 
 	private List<ReservationView> reservations = new ArrayList<ReservationView>();
 
@@ -49,23 +55,56 @@ public class ReservationsHandler implements Serializable {
 	}
 
 	public String reservations() {
-		System.out.println("Reservations Entered");
 
 		ReservationService rs = serviceLocator.getReservationService();
 
-		System.out.println("Service found");
 		reservations = rs.getReservationsForCustomer(loginHandler.getCustomer());
 		if (!reservations.isEmpty()) {
-			System.out.println("Reservations Found");
+
+			reservationValues = new String[reservations.size()];
+			List<String> help = new ArrayList<String>();
+			for (ReservationView r : reservations) {
+				help.add(r.getReservation_id().toString());
+			}
+
+			reservationValues = help.toArray(reservationValues);
+
 			return "success";
 		}
-		System.out.println("No Reservations Found");
 
 		return "noRes";
 	}
 
 	public String changeReservation() {
-		return "success";
+
+		reservation = reservations.get(Integer.parseInt(selectedReservation) - 1);
+
+		if (reservation != null) {
+			travellers = reservation.getTravellerNames().size();
+
+			return "success";
+		}
+
+		return "NoSuccess";
+	}
+
+	public void changeTraveller(ValueChangeEvent ev) {
+		travellers = (Integer) ev.getNewValue();
+		ArrayList<TravellerView> help = new ArrayList<TravellerView>(travellers);
+		for (int i = 0; i < travellers; i++)
+			help.add(new TravellerView(i, ""));
+		reservation.setTravellerNames(help);
+		FacesContext.getCurrentInstance().renderResponse();
+	}
+
+	public void changeReservation(ValueChangeEvent ev) {
+		selectedReservation = (String) ev.getNewValue();
+
+		FacesContext.getCurrentInstance().renderResponse();
+	}
+
+	public int[] getTravellerValues() {
+		return new int[] { 0, 1, 2, 3, 4, 5, 6 };
 	}
 
 	public List<ReservationView> getReservations() {
@@ -76,11 +115,35 @@ public class ReservationsHandler implements Serializable {
 		this.reservations = reservations;
 	}
 
-	public int getSelectedReservation() {
+	public String getSelectedReservation() {
 		return selectedReservation;
 	}
 
-	public void setSelectedReservation(int selectedReservation) {
+	public void setSelectedReservation(String selectedReservation) {
 		this.selectedReservation = selectedReservation;
+	}
+
+	public String[] getReservationValues() {
+		return reservationValues;
+	}
+
+	public void setReservationValues(String[] reservationValues) {
+		this.reservationValues = reservationValues;
+	}
+
+	public ReservationView getReservation() {
+		return reservation;
+	}
+
+	public void setReservation(ReservationView reservation) {
+		this.reservation = reservation;
+	}
+
+	public int getTravellers() {
+		return travellers;
+	}
+
+	public void setTravellers(int travellers) {
+		this.travellers = travellers;
 	}
 }
